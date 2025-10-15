3b55e763a993c646f3a13e3ba84a2c81ab4d00e8
Divyankar Shah <divyankarshah1602@gmail.com>
2025-10-15 22:22:13 +0530

    Fix: Updated middleware.ts file

--- tonex-labs-frontend/src/middleware.ts
+++ tonex-labs-frontend/src/middleware.ts
@@ -1,32 +1,65 @@
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { auth } from "~/server/auth";
+
+// export async function middleware(request: NextRequest) {
+//     const hasSession = await auth();
+
+//     const path = request.nextUrl.pathname;
+
+//     const isAuthRoute = path === "/app/sign-in" || path === "/app/sign-up";
+
+//     const isProtectedRoute = path.startsWith("/app/") && !isAuthRoute;
+
+//     if(hasSession && isAuthRoute) {
+//         return NextResponse.redirect(new URL("/app/speech-synthesis/text-to-speech", request.url))
+//     }
+
+//     if(!hasSession && isProtectedRoute){
+//         const signInUrl = new URL("/app/sign-in", request.url);
+//         signInUrl.searchParams.set("callbackUrl", request.url);
+
+//         return NextResponse.redirect(signInUrl);
+//     }
+
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: [
+//         "/app/:path*"
+//     ],
+// }
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { auth } from "~/server/auth";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-    const hasSession = await auth();
+  const token = await getToken({ 
+    req: request,
+    secret: process.env.AUTH_SECRET 
+  });
+  
+  const path = request.nextUrl.pathname;
+  const isAuthRoute = path === "/app/sign-in" || path === "/app/sign-up";
+  const isProtectedRoute = path.startsWith("/app/") && !isAuthRoute;
 
-    const path = request.nextUrl.pathname;
+  if (token && isAuthRoute) {
+    return NextResponse.redirect(
+      new URL("/app/speech-synthesis/text-to-speech", request.url)
+    );
+  }
 
-    const isAuthRoute = path === "/app/sign-in" || path === "/app/sign-up";
+  if (!token && isProtectedRoute) {
+    const signInUrl = new URL("/app/sign-in", request.url);
+    signInUrl.searchParams.set("callbackUrl", request.url);
+    return NextResponse.redirect(signInUrl);
+  }
 
-    const isProtectedRoute = path.startsWith("/app/") && !isAuthRoute;
-
-    if(hasSession && isAuthRoute) {
-        return NextResponse.redirect(new URL("/app/speech-synthesis/text-to-speech", request.url))
-    }
-
-    if(!hasSession && isProtectedRoute){
-        const signInUrl = new URL("/app/sign-in", request.url);
-        signInUrl.searchParams.set("callbackUrl", request.url);
-
-        return NextResponse.redirect(signInUrl);
-    }
-
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        "/app/:path*"
-    ],
-}
+  matcher: ["/app/:path*"],
+};
